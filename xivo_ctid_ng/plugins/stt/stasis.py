d5d7404b5cf1354da52785d48aa64dd97d3d7089
Mehdi Abaakouk <sileht@sileht.net>
2019-01-23 10:32:04 +0100

    handle when google sent no reponse

--- xivo_ctid_ng/plugins/stt/stasis.py
+++ xivo_ctid_ng/plugins/stt/stasis.py
@@ -102,11 +102,13 @@ class SttStasis:
         responses = list(self._speech_client.streaming_recognize(
             self._streaming_config, [request]))
 
-        results = list(responses[0].results)
-        logger.critical("results: %d" % len(results))
-        for result in results:
-            if result.is_final:
-                result_stt = result.alternatives[0].transcript
-                logger.critical("test: %s", result_stt)
-                channel.setChannelVar(variable="X_WAZO_STT", value=result_stt)
-                self._notifier.publish_stt(channel.id, result_stt)
+        logger.critical("responses: %d" % len(responses))
+        for response in responses:
+            results = list(response.results)
+            logger.critical("results: %d" % len(results))
+            for result in results:
+                if result.is_final:
+                    result_stt = result.alternatives[0].transcript
+                    logger.critical("test: %s", result_stt)
+                    channel.setChannelVar(variable="X_WAZO_STT", value=result_stt)
+                    self._notifier.publish_stt(channel.id, result_stt)
